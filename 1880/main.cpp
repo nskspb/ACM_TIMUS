1f05b927aec0560894202ddc4f4a517ca7db181a
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-11-20 17:25:39 +0300

    Accepted

--- 1880/main.cpp
+++ 1880/main.cpp
@@ -16,15 +16,32 @@ int main()
             std::cin >> ch[j][i];
         }
     }
-
-    std::vector<int> s(std::max(n[0], std::max(n[1], n[2])));
+    int max = std::max(n[0], std::max(n[1], n[2]));
+    int NumMax = 0;
+    for (int i = 0; i < 3; ++i)
+    {
+        if (n[i] == max)
+        {
+            NumMax = i;
+        }
+    }
+    std::sort(ch[NumMax].begin(), ch[NumMax].end());
+    for (int i = 0; i < ch[NumMax].size() - 1; ++i)
+    {
+        if (ch[NumMax][i] == ch[NumMax][i + 1])
+        {
+            ch[NumMax].erase(ch[NumMax].begin() + i);
+            max--;
+        }
+    }
+    std::vector<int> s(max);
     bool check = false;
     for (int j = 0; j < s.size(); ++j)
     {
         check = false;
         for (auto i = 0; i < n[1]; ++i)
         {
-            if ((ch[1][i] == ch[0][j]) && (check == false))
+            if ((ch[1][i] == ch[NumMax][j]) && (check == false))
             {
                 s[j]++;
                 check = true;
@@ -37,36 +54,35 @@ int main()
         check = false;
         for (auto i = 0; i < n[2]; ++i)
         {
-            if ((ch[2][i] == ch[0][j]) && (check == false))
+            if ((ch[2][i] == ch[NumMax][j]) && (check == false))
             {
                 s[j]++;
                 check = true;
             }
         }
     }
+
+    for (int j = 0; j < s.size(); ++j)
+    {
+        check = false;
+        for (auto i = 0; i < n[0]; ++i)
+        {
+            if ((ch[0][i] == ch[NumMax][j]) && (check == false))
+            {
+                s[j]++;
+                check = true;
+            }
+        }
+    }
+
     int k = 0;
     for (int j = 0; j < s.size(); ++j)
     {
-        if (s[j] >= 2)
+        if (s[j] >= 3)
         {
             k++;
         }
     }
-
     std::cout << k;
-    /*for (auto j = 0; j < 3; ++j)
-    {
-        for (auto i = 0; i < n[j]; ++i)
-        {
-            std::cout << ch[j][i];
-        }
-        std::cout << std::endl;
-    }
-
-    for (int j = 0; j < s.size(); ++j)
-    {
-        std::cout << s[j];
-    }
-    std::cout << std::endl;*/
     return 0;
 }
