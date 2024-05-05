0e5c1e4c17ceccfa7c1a50aee960fd7fcf7664e9
Ruslan <nugaev.ruslan2010@yandex.ru>
2024-05-05 15:59:32 +0300

    success

--- 1225/main.cpp
+++ 1225/main.cpp
@@ -4,8 +4,7 @@ int main()
 {
     int N;
     std::cin >> N;
-    int summa = 0;
-    int mas[N];
+    long long mas[N];
     mas[0] = 2;
     mas[1] = 2;
 
@@ -16,11 +15,6 @@ int main()
             mas[i] = mas[i - 1] + mas[i - 2];
         }
     }
-
-    /*for (auto i = 0; i < N; ++i)
-    {
-        std::cout << mas[i] << "  ";
-    }*/
     std::cout << mas[N - 1] << std::endl;
     return 0;
 }
